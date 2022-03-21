69ff8a3c9b885ab52329d7c55450c94cabc7f09b
Daniel Castillejo <devcastillejo@gmail.com>
2022-03-21 15:12:50 -0500

    Adds prisma schema

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,12 @@
 import type { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
@@ -11,6 +16,16 @@ export const authOptions: NextAuthOptions = {
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
       return true
+    },
+    async session({ session, user }) {
+      return {
+        ...session,
+        User: {
+          ...session.user,
+          id: user.id,
+          role: user.role
+        }
+      }
     }
   },
   pages: {
