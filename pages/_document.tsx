609e91077f8ebb081f5bc91f835fb0cae55a83c1
Daniel Castillejo <devcastillejo@gmail.com>
2022-05-16 00:12:29 -0500

    Adds favicon for multiple devices

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,27 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400;600;700&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f84e32" />
+          <meta name="msapplication-TileColor" content="#603cba" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body className="h-full overflow-hidden">
           <Main />
