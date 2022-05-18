2c3dcc6fbcf2118ea49a4b339d27cccaac63484d
Daniel Castillejo <devcastillejo@gmail.com>
2022-05-18 15:11:10 -0500

    Implements warning for unsaved changes

--- hooks/useWarnChanges.ts
+++ hooks/useWarnChanges.ts
@@ -1,21 +1,32 @@
 import Router from "next/router"
 import { useEffect } from "react"
 
-const useWarnChanges = (unsavedChanges: boolean, callback: () => boolean) => {
+const useWarnChanges = (
+  unsavedChanges: boolean,
+  message = "Tiene cambios sin guardar - ¿Está seguro de abandonar esta página?"
+) => {
   useEffect(() => {
-    if (unsavedChanges) {
-      const routeChangeStart = () => {
-        const ok = callback()
-        if (!ok) {
-          Router.events.emit("routeChangeError")
-          throw "Abort route change. Please ignore this error."
-        }
+    const handleWindowClose = (e: BeforeUnloadEvent) => {
+      if (!unsavedChanges) return
+      e.preventDefault()
+      return (e.returnValue = message)
+    }
+    const handleBrowseAway = () => {
+      if (!unsavedChanges) return
+      if (window.confirm(message)) return
+      Router.events.emit("routeChangeError")
+      //push state, because browser back action changes link and changes history state
+      // but we stay on the same page
+      if (Router.asPath !== window.location.pathname) {
+        window.history.pushState("", "", Router.asPath)
       }
-      Router.events.on("routeChangeStart", routeChangeStart)
-
-      return () => {
-        Router.events.off("routeChangeStart", routeChangeStart)
-      }
+      throw "routeChange aborted."
+    }
+    window.addEventListener("beforeunload", handleWindowClose)
+    Router.events.on("routeChangeStart", handleBrowseAway)
+    return () => {
+      window.removeEventListener("beforeunload", handleWindowClose)
+      Router.events.off("routeChangeStart", handleBrowseAway)
     }
   }, [unsavedChanges])
 }
