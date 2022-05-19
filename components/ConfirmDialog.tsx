596fdfdece41bd8e9b8c83ee8e8a5f50ca116154
Daniel Castillejo <devcastillejo@gmail.com>
2022-05-19 15:06:42 -0500

    Adds empty state for items page

--- components/ConfirmDialog.tsx
+++ components/ConfirmDialog.tsx
@@ -1,75 +1,113 @@
 import React, { Fragment } from "react"
 import * as AlertDialog from "@radix-ui/react-alert-dialog"
 import { ExclamationIcon } from "@heroicons/react/outline"
-import { Transition } from "@headlessui/react"
+import { AnimatePresence, motion } from "framer-motion"
 
 import Button from "@/components/Button"
+
+const overlay = {
+  visible: {
+    opacity: 1,
+    transition: {
+      ease: "easeOut",
+      duration: 0.2
+    }
+  },
+  hidden: {
+    opacity: 0,
+    transition: {
+      ease: "easeIn",
+      duration: 0.1
+    }
+  }
+}
+
+const dialog = {
+  visible: {
+    opacity: 1,
+    y: 0,
+    scale: 1,
+    transition: {
+      ease: "easeOut",
+      duration: 0.2
+    }
+  },
+  hidden: {
+    opacity: 0,
+    y: 4,
+    scale: 0.95,
+    transition: {
+      ease: "easeIn",
+      duration: 0.2
+    }
+  }
+}
 
 const ConfirmDialog = ({ title, children, open, setOpen, onConfirm }) => {
   return (
     <AlertDialog.Root open={open} onOpenChange={open => setOpen(open)}>
-      <AlertDialog.Portal>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <AlertDialog.Overlay className="fixed inset-0 z-50 min-h-screen bg-black bg-opacity-30 px-4 pt-4 pb-20 transition-opacity sm:block sm:p-0" />
-        </Transition.Child>
-        <div className="fixed inset-0 z-50 flex min-h-screen items-end justify-center overflow-y-auto px-4 pt-4 pb-20 sm:items-start">
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enterTo="opacity-100 translate-y-0 sm:scale-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-          >
-            <AlertDialog.Content className="inline-block transform overflow-hidden rounded-xl bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6 sm:align-middle">
-              <div className="sm:flex sm:items-start">
-                <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                  <ExclamationIcon
-                    className="h-6 w-6 text-red-600"
-                    aria-hidden="true"
-                  />
-                </div>
-                <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                  <AlertDialog.Title className="text-lg font-medium leading-6 text-gray-900">
-                    {title}
-                  </AlertDialog.Title>
-                  <div className="mt-2">
-                    <AlertDialog.Description className="text-sm text-gray-500">
-                      {children}
-                    </AlertDialog.Description>
+      <AnimatePresence>
+        {open ? (
+          <AlertDialog.Portal>
+            <AlertDialog.Overlay asChild forceMount>
+              <motion.div
+                initial="hidden"
+                animate="visible"
+                exit="hidden"
+                variants={overlay}
+                className="fixed inset-0 z-50 min-h-screen bg-black bg-opacity-30 px-4 pt-4 pb-20 sm:block sm:p-0"
+              />
+            </AlertDialog.Overlay>
+            <div className="fixed inset-0 z-50 flex min-h-screen items-end justify-center overflow-y-auto px-4 pt-4 pb-20 sm:items-start">
+              <AlertDialog.Content asChild forceMount>
+                <motion.div
+                  initial="hidden"
+                  animate="visible"
+                  exit="hidden"
+                  variants={dialog}
+                  className="relative inline-block transform overflow-hidden rounded-xl bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl sm:my-8 sm:w-full sm:max-w-lg sm:p-6 sm:align-middle"
+                >
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <ExclamationIcon
+                        className="h-6 w-6 text-red-600"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <AlertDialog.Title className="text-lg font-medium leading-6 text-gray-900">
+                        {title}
+                      </AlertDialog.Title>
+                      <div className="mt-2">
+                        <AlertDialog.Description className="text-sm text-gray-500">
+                          {children}
+                        </AlertDialog.Description>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div className="mt-5 flex flex-col gap-2 sm:mt-4 sm:flex-row-reverse">
-                <AlertDialog.Action asChild>
-                  <Button
-                    type="button"
-                    size="sm"
-                    variant="danger"
-                    onClick={() => onConfirm()}
-                  >
-                    Eliminar
-                  </Button>
-                </AlertDialog.Action>
-                <AlertDialog.Cancel asChild>
-                  <Button type="button" size="sm" variant="secondary">
-                    Cancelar
-                  </Button>
-                </AlertDialog.Cancel>
-              </div>
-            </AlertDialog.Content>
-          </Transition.Child>
-        </div>
-      </AlertDialog.Portal>
+                  <div className="mt-5 flex flex-col gap-2 sm:mt-4 sm:flex-row-reverse">
+                    <AlertDialog.Action asChild>
+                      <Button
+                        type="button"
+                        size="sm"
+                        variant="danger"
+                        onClick={() => onConfirm()}
+                      >
+                        Eliminar
+                      </Button>
+                    </AlertDialog.Action>
+                    <AlertDialog.Cancel asChild>
+                      <Button type="button" size="sm" variant="secondary">
+                        Cancelar
+                      </Button>
+                    </AlertDialog.Cancel>
+                  </div>
+                </motion.div>
+              </AlertDialog.Content>
+            </div>
+          </AlertDialog.Portal>
+        ) : null}
+      </AnimatePresence>
     </AlertDialog.Root>
   )
 }
