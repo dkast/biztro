bea2164987f93144dd8956f82f2a330ac41f5bf1
Daniel Castillejo <devcastillejo@gmail.com>
2022-05-30 20:28:31 -0500

    Adds section for main features

--- pages/home.tsx
+++ pages/home.tsx
@@ -12,9 +12,9 @@ const Home: NextPageWithAuthAndLayout = () => {
         <title>Bistro</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex h-screen flex-col items-center px-2">
+      <div className="flex h-screen flex-col items-center overflow-auto">
         {/* Menu */}
-        <div className="mt-2 flex w-full max-w-6xl items-center bg-gray-100 py-2">
+        <div className="mt-2 flex w-full max-w-6xl items-center py-2 px-2 xl:px-0">
           <Image src="/logo-bistro.svg" alt="Logo" width={40} height={40} />
           <Toolbar.Root className="ml-auto">
             <Toolbar.Button>
@@ -25,7 +25,7 @@ const Home: NextPageWithAuthAndLayout = () => {
           </Toolbar.Root>
         </div>
         {/* Hero */}
-        <div className="bg-50 h- grid w-full max-w-6xl sm:grid-cols-2">
+        <div className="grid w-full max-w-6xl px-2 sm:grid-cols-2 xl:px-0">
           <div className="py-12 md:py-24">
             <h1 className="text-6xl font-bold">Tu menú digital en minutos</h1>
             <h2 className="py-6 text-2xl text-gray-600 md:py-8">
@@ -39,7 +39,7 @@ const Home: NextPageWithAuthAndLayout = () => {
                 </a>
               </Link>
               <Link href="/app/dashboard">
-                <a className="rounded-lg border border-orange-500 px-4 py-3 text-orange-600">
+                <a className="rounded-lg border border-orange-500 px-4 py-3 text-orange-600 hover:bg-orange-50">
                   Ver un Ejemplo
                 </a>
               </Link>
@@ -48,7 +48,54 @@ const Home: NextPageWithAuthAndLayout = () => {
           <div className="bg-gray-200"></div>
         </div>
         {/* Main */}
-        <div className="h-80 w-full bg-gray-100"></div>
+        <div className="flex w-full flex-col justify-center bg-gray-100">
+          {/* QR */}
+          <section className="mx-auto grid w-full max-w-6xl px-2 md:grid-cols-2 xl:px-0">
+            <div className="flex flex-col justify-center p-4">
+              <h3 className="text-2xl font-bold">Obtén tu Código QR</h3>
+              <p>
+                Crea tu menú que permita que tus clientes consultarlo
+                rápidamente utilizando simplementa la cámara en su télefono
+                móvil.
+              </p>
+            </div>
+            <div>Imagen 1</div>
+          </section>
+          {/* Online */}
+          <section className="mx-auto grid w-full max-w-6xl px-2 md:grid-cols-2 xl:px-0">
+            <div className="col-start-1 flex flex-col justify-center p-4 md:col-start-2">
+              <h3 className="text-2xl font-bold">No requiere instalación</h3>
+              <p>
+                Tu menú esta disponible para todos, no se requiere instalar
+                alguna app, puede verse desde tú teléfono, tablet o escritorio.
+              </p>
+            </div>
+            <div className="md:order-first">Imagen 2</div>
+          </section>
+          {/* Customize */}
+          <section className="mx-auto grid w-full max-w-6xl px-2 md:grid-cols-2 xl:px-0">
+            <div className="flex flex-col justify-center p-4">
+              <h3 className="text-2xl font-bold">Diseño flexible</h3>
+              <p>
+                Inicia con una plantilla moderna, personalizala a tu gusto para
+                crear algo original justo como tu negocio.
+              </p>
+            </div>
+            <div>Imagen 3</div>
+          </section>
+          {/* Social */}
+          <section className="mx-auto grid w-full max-w-6xl px-2 md:grid-cols-2 xl:px-0">
+            <div className="col-start-1 flex flex-col justify-center p-4 md:col-start-2">
+              <h3 className="text-2xl font-bold">Haz cambios al instante</h3>
+              <p>
+                Con una interfaz de arrastrar y soltar, es fácil cambios a tu
+                menú, no requieres habilidades técnicas y los resultados se
+                pueden ver al instante.
+              </p>
+            </div>
+            <div className="md:order-first">Imagen 4</div>
+          </section>
+        </div>
       </div>
     </>
   )
