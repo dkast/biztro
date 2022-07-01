4baade2e91ed4eb260fa43885e0ce510634b7ac8
Daniel Castillejo <devcastillejo@gmail.com>
2022-06-30 21:02:38 -0500

    Fixe minor layout and visual issues

--- components/marketing/MainMenu.tsx
+++ components/marketing/MainMenu.tsx
@@ -10,7 +10,11 @@ interface MainMenuProps {
 const MainMenu = ({ variant }: MainMenuProps): JSX.Element => {
   return (
     <div className="mt-2 flex w-full max-w-6xl items-center py-2 px-4 lg:px-2 xl:px-0">
-      <Image src="/logo-bistro.svg" alt="Logo" width={40} height={40} />
+      <Link href="/">
+        <a>
+          <Image src="/logo-bistro.svg" alt="Logo" width={40} height={40} />
+        </a>
+      </Link>
       <Toolbar.Root className="ml-auto">
         <Toolbar.Button asChild>
           <Link href="/app/dashboard">
@@ -18,7 +22,7 @@ const MainMenu = ({ variant }: MainMenuProps): JSX.Element => {
               className={classNames(
                 "rounded-lg border px-3 py-2 text-sm transition",
                 variant === "dark"
-                  ? "border-violet-600 bg-violet-500/30  text-violet-300 hover:bg-violet-300"
+                  ? "border-violet-600 bg-violet-500/10  text-violet-300 hover:bg-violet-500/30"
                   : "border-gray-300 text-violet-500 hover:bg-violet-50"
               )}
             >
