e9ca187645e8a7fa37d25e8f88fc82d4bd47f91b
Daniel Castillejo <devcastillejo@gmail.com>
2022-07-13 20:32:54 -0500

    Adds util to download image

--- components/editor/QREditor.tsx
+++ components/editor/QREditor.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link"
+import { useRef } from "react"
 import { useRecoilValue } from "recoil"
 import { QRCode } from "react-qrcode-logo"
 
 import { hostState } from "@/lib/store"
 import Button from "@/components/Button"
+import exportAsImage from "@/lib/export-as-image"
 
 interface QREditorProps {
   siteId: string
@@ -11,25 +13,37 @@ interface QREditorProps {
 
 const QREditor = ({ siteId }: QREditorProps): JSX.Element => {
   const host = useRecoilValue(hostState)
+  const exportRef = useRef()
   return (
     <>
       <h3 className="text-lg font-medium leading-6 text-gray-900">Editor QR</h3>
       <div className="flex flex-col items-center justify-center">
-        <span className="my-2 text-gray-500">
-          Escanea con la cámara de tu móvil o aplicación QR o sigue{" "}
-          <Link href="/app/site-preview" passHref>
+        <span className="mt-2 text-sm text-gray-500">
+          Al escanear el código con la cámara de tu móvil o aplicación QR te
+          llevará a la siguiente dirección:{" "}
+          <Link href={`${host}/${siteId}`} passHref>
             <a
               className="text-violet-500 hover:text-violet-700"
               target="_blank"
               rel="noopener noreferrer"
             >
-              esta liga.
+              {`${host}/${siteId}`}
             </a>
           </Link>
         </span>
-        <QRCode value={`${host}/${siteId}`} />
-        <div>
-          <Button variant="secondary">Guardar</Button>
+        <div className="my-6 rounded-lg border-2 border-dashed border-gray-300">
+          <div ref={exportRef} className="p-1">
+            <QRCode value={`${host}/${siteId}`} />
+          </div>
+        </div>
+        <div className="w-full">
+          <Button
+            variant="primary"
+            mode="full"
+            onClick={() => exportAsImage(exportRef.current, siteId)}
+          >
+            Descargar imagen QR
+          </Button>
         </div>
       </div>
     </>
