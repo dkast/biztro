5d5baf48f329b0e081895492f552ea1392d2be20
Daniel Castillejo <devcastillejo@gmail.com>
2024-05-21 20:21:33 -0500

    style: restyle project view

--- src/app/dashboard/menu-list.tsx
+++ src/app/dashboard/menu-list.tsx
@@ -5,9 +5,8 @@ import type { Menu } from "@prisma/client"
 import { AnimatePresence, motion } from "framer-motion"
 import { CircleCheck, MoreHorizontal } from "lucide-react"
 import Link from "next/link"
+import gradient from "random-gradient"
 
-import InfoHelper from "@/components/dashboard/info-helper"
-import { TooltipHelper } from "@/components/dashboard/tooltip-helper"
 import { AlertDialog } from "@/components/ui/alert-dialog"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
@@ -42,6 +41,7 @@ export default function MenuList({ menus }: { menus: Menu[] }) {
 
 function MenuCard({ menu, index }: { menu: Menu; index: number }) {
   const [openDelete, setOpenDelete] = useState<boolean>(false)
+  const bgGradient = { background: gradient(menu.id) }
   return (
     <motion.div
       layout
@@ -49,78 +49,87 @@ function MenuCard({ menu, index }: { menu: Menu; index: number }) {
       exit={{ scale: 0.8, opacity: 0 }}
       transition={{ type: "spring" }}
     >
-      <Link href={`/menu-editor/${menu.id}`} prefetch={false}>
-        <motion.div
-          whileHover={{ y: -6 }}
-          whileTap={{ scale: 0.98 }}
-          className="flex h-[370px] items-center justify-center gap-4 rounded-lg bg-white shadow-lg"
+      <motion.div
+        whileHover={{ scale: 1.02 }}
+        whileTap={{ scale: 0.98 }}
+        className="grid h-[250px] grid-rows-5 overflow-hidden rounded-lg shadow-lg"
+      >
+        <Link
+          href={`/menu-editor/${menu.id}`}
+          prefetch={false}
+          className="row-span-3 flex items-center justify-center"
+          style={bgGradient}
         >
           <img
             src="safari-pinned-tab.svg"
             alt={menu.name}
             className="size-16 opacity-10"
           />
-        </motion.div>
-      </Link>
-      <div className="flex flex-row justify-between gap-2 py-3">
-        <h2>{menu.name}</h2>
-        <div className="flex flex-row items-center gap-1">
-          {(() => {
-            switch (menu.status) {
-              case MenuStatus.PUBLISHED:
-                return (
-                  <Badge variant="blue" className="rounded-full">
-                    Publicado
-                  </Badge>
-                )
-              case MenuStatus.DRAFT:
-                return (
-                  <Badge variant="secondary" className="rounded-full">
-                    Borrador
-                  </Badge>
-                )
-              default:
-                return null
-            }
-          })()}
+        </Link>
+        <div className="row-span-2 flex flex-col justify-between gap-2 rounded-b-lg bg-white px-4 py-3">
+          <Link href={`/menu-editor/${menu.id}`} prefetch={false}>
+            <h2 className="font-medium">{menu.name}</h2>
+          </Link>
+          <div className="flex flex-row items-center justify-between gap-1">
+            <div className="flex flex-row gap-1">
+              {(() => {
+                switch (menu.status) {
+                  case MenuStatus.PUBLISHED:
+                    return (
+                      <Badge variant="blue" className="rounded-full">
+                        Publicado
+                      </Badge>
+                    )
+                  case MenuStatus.DRAFT:
+                    return (
+                      <Badge variant="secondary" className="rounded-full">
+                        Borrador
+                      </Badge>
+                    )
+                  default:
+                    return null
+                }
+              })()}
 
-          {index === 0 && menu.status === MenuStatus.PUBLISHED && (
-            <Badge
-              variant="green"
-              className="flex items-center justify-between gap-1 rounded-full px-1.5"
-            >
-              <CircleCheck className="size-3" />
-              Activo
-            </Badge>
-          )}
-
-          <AlertDialog>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="size-8 text-gray-700"
+              {index === 0 && menu.status === MenuStatus.PUBLISHED && (
+                <Badge
+                  variant="green"
+                  className="flex items-center justify-between gap-1 rounded-full px-1.5"
                 >
-                  <MoreHorizontal className="size-3.5" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end" className="w-32">
-                <DropdownMenuLabel>Acciones</DropdownMenuLabel>
-                <DropdownMenuItem asChild>
-                  <Link href={`/menu-editor/${menu.id}`} prefetch={false}>
-                    <span>Editar</span>
-                  </Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setOpenDelete(true)}>
-                  <span className="text-red-500">Eliminar</span>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </AlertDialog>
-          <MenuDelete menu={menu} open={openDelete} setOpen={setOpenDelete} />
+                  <CircleCheck className="size-3" />
+                  Activo
+                </Badge>
+              )}
+            </div>
+
+            <AlertDialog>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    className="size-8 text-gray-700"
+                  >
+                    <MoreHorizontal className="size-3.5" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="w-32">
+                  <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+                  <DropdownMenuItem asChild>
+                    <Link href={`/menu-editor/${menu.id}`} prefetch={false}>
+                      <span>Editar</span>
+                    </Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => setOpenDelete(true)}>
+                    <span className="text-red-500">Eliminar</span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </AlertDialog>
+            <MenuDelete menu={menu} open={openDelete} setOpen={setOpenDelete} />
+          </div>
         </div>
-      </div>
+      </motion.div>
     </motion.div>
   )
 }
