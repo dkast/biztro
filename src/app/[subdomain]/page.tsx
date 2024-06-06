26f1ad6dd0acad59f5006c29bca1751b8989402e
Daniel Castillejo <devcastillejo@gmail.com>
2024-06-05 23:48:52 -0500

    feat: add function for dynamic theme color in site page closes BIZ-67

--- src/app/[subdomain]/page.tsx
+++ src/app/[subdomain]/page.tsx
@@ -1,6 +1,7 @@
 import { rgbaToHex, type RgbaColor } from "@uiw/react-color"
 import lz from "lzutf8"
 import type { Metadata, ResolvingMetadata } from "next"
+import { type Viewport } from "next"
 import { notFound } from "next/navigation"
 
 import ResolveEditor from "@/app/[subdomain]/resolve-editor"
@@ -35,6 +36,52 @@ export async function generateMetadata(
     }
   }
 }
+
+export async function generateViewport({
+  params
+}: {
+  params: { subdomain: string }
+}): Promise<Viewport> {
+  const siteMenu = await getMenuByOrgSubdomain(params.subdomain)
+
+  if (!params.subdomain || !siteMenu) {
+    return {
+      themeColor: "#000000"
+    }
+  }
+
+  let json
+  if (siteMenu.serialData) {
+    json = lz.decompress(lz.decodeBase64(siteMenu.serialData))
+  }
+
+  let backgroundColor: RgbaColor = { r: 255, g: 255, b: 255, a: 1 }
+
+  // Search container style (color)
+  const data = JSON.parse(json)
+  const keys = Object.keys(data)
+  keys.forEach(el => {
+    const node = data[el]
+    const { displayName } = node
+    if (displayName === "Sitio") {
+      backgroundColor = data[el]?.props?.backgroundColor
+    }
+  })
+
+  if (backgroundColor) {
+    return {
+      themeColor: rgbaToHex(backgroundColor)
+    }
+  } else {
+    return {
+      themeColor: "#000000"
+    }
+  }
+}
+
+// export const viewport: Viewport = {
+//   themeColor: "#000000"
+// }
 
 export default async function SitePage({
   params
