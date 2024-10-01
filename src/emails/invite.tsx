7fa4e978b940768912a300607a2b2658104f7c60
Daniel Castillejo <devcastillejo@gmail.com>
2024-09-30 20:53:43 -0500

    chore: add and configure resend and react-email

---
+++ src/emails/invite.tsx
@@ -0,0 +1,111 @@
+import * as React from "react"
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Section,
+  Tailwind,
+  Text
+} from "@react-email/components"
+
+interface VercelInviteUserEmailProps {
+  username?: string
+  userImage?: string
+  invitedByUsername?: string
+  invitedByEmail?: string
+  teamName?: string
+  teamImage?: string
+  inviteLink?: string
+  inviteFromIp?: string
+  inviteFromLocation?: string
+}
+
+const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : ""
+
+export const BiztroInviteUserEmail = ({
+  username,
+  invitedByUsername,
+  invitedByEmail,
+  teamName,
+  inviteLink
+}: VercelInviteUserEmailProps) => {
+  const previewText = `Unete a ${invitedByUsername} en Biztro`
+
+  return (
+    <Html>
+      <Head />
+      <Preview>{previewText}</Preview>
+      <Tailwind>
+        <Body className="mx-auto my-auto bg-white px-2 font-sans">
+          <Container className="mx-auto my-[40px] max-w-[465px] rounded border border-solid border-[#eaeaea] p-[20px]">
+            <Section className="mt-[32px]">
+              <Img
+                src={`${baseUrl}/static/logo.png`}
+                width="40"
+                height="37"
+                alt="Biztro"
+                className="mx-auto my-0"
+              />
+            </Section>
+            <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-black">
+              Unete a <strong>{teamName}</strong> en <strong>Biztro</strong>
+            </Heading>
+            <Text className="text-[14px] leading-[24px] text-black">
+              Hola {username},
+            </Text>
+            <Text className="text-[14px] leading-[24px] text-black">
+              <strong>{invitedByUsername}</strong> (
+              <Link
+                href={`mailto:${invitedByEmail}`}
+                className="text-orange-600 no-underline"
+              >
+                {invitedByEmail}
+              </Link>
+              ) te ha invitado al equipo de <strong>{teamName}</strong> en{" "}
+              <strong>Biztro</strong>.
+            </Text>
+            <Section className="mb-[32px] mt-[32px] text-center">
+              <Button
+                className="rounded bg-[#171717] px-5 py-3 text-center text-[12px] font-semibold text-white no-underline"
+                href={inviteLink}
+              >
+                Unirse al equipo
+              </Button>
+            </Section>
+            <Text className="text-[14px] leading-[24px] text-black">
+              o copia y pega esta URL en tu navegador:{" "}
+              <Link href={inviteLink} className="text-orange-600 no-underline">
+                {inviteLink}
+              </Link>
+            </Text>
+            <Hr className="mx-0 my-[26px] w-full border border-solid border-[#eaeaea]" />
+            <Text className="text-[12px] leading-[24px] text-[#666666]">
+              Esta invitacion esta destinada para{" "}
+              <span className="text-black">{username}</span>. Si no estabas
+              esperando una invitacion, puedes ignorar este email.
+            </Text>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  )
+}
+
+BiztroInviteUserEmail.PreviewProps = {
+  username: "alanturing",
+  invitedByUsername: "Alan",
+  invitedByEmail: "alan.turing@example.com",
+  teamName: "Enigma",
+  inviteLink: "https://vercel.com/teams/invite/foo"
+} as VercelInviteUserEmailProps
+
+export default BiztroInviteUserEmail
