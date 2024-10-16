07002a4bc949fa4a53e768b6e2d80e511c553672
Daniel Castillejo <devcastillejo@gmail.com>
2024-10-15 20:25:45 -0500

    refactor: update container block to conditionally render background gradient

--- src/components/menu-editor/blocks/container-block.tsx
+++ src/components/menu-editor/blocks/container-block.tsx
@@ -45,7 +45,9 @@ export default function ContainerBlock({
           backgroundPosition: backgroundImage?.split("-")[1]
         }}
       >
-        <div className="absolute inset-0 bg-gradient-to-b from-black/20 to-black/80"></div>
+        {backgroundImage?.startsWith("bg") && (
+          <div className="absolute inset-0 bg-gradient-to-b from-black/20 to-black/80"></div>
+        )}
       </div>
       <main className="mx-auto flex max-w-screen-md grow flex-col @container">
         <div className="relative flex grow flex-col pb-8">{children}</div>
