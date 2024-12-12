fa93adc06c55e1554e1815f51338865073c39cad
Daniel Castillejo <devcastillejo@gmail.com>
2024-12-11 19:30:55 -0600

    chore: clean up FeaturedBlock component by removing unused ItemView and console log

--- src/components/menu-editor/blocks/featured-block.tsx
+++ src/components/menu-editor/blocks/featured-block.tsx
@@ -5,7 +5,6 @@ import type { Prisma } from "@prisma/client"
 import type { RgbaColor } from "@uiw/react-color"
 
 import FeaturedSettings from "@/components/menu-editor/blocks/featured-settings"
-import { ItemView } from "@/components/menu-editor/blocks/item-block"
 import FontWrapper from "@/components/menu-editor/font-wrapper"
 import { Card, CardContent } from "@/components/ui/card"
 import {
@@ -56,7 +55,6 @@ export default function FeaturedBlock({
     isEditing: state.options.enabled
   }))
 
-  console.log(items)
   if (!items?.length) return <div>No hay elementos destacados</div>
 
   return (
@@ -80,24 +78,6 @@ export default function FeaturedBlock({
             <CarouselItem key={item.id} className="md:basis-1/2 lg:basis-1/3">
               <Card>
                 <CardContent className="p-0">
-                  {/* <ItemView
-                    item={item}
-                    {...{
-                      backgroundMode,
-                      itemFontSize,
-                      itemColor,
-                      itemFontWeight,
-                      itemFontFamily,
-                      priceFontSize,
-                      priceColor,
-                      priceFontWeight,
-                      priceFontFamily,
-                      descriptionFontFamily,
-                      descriptionColor,
-                      showImage,
-                      isEditing
-                    }}
-                  /> */}
                   <div className="relative h-40 overflow-hidden rounded-lg p-4">
                     {item.image ? (
                       <div
@@ -117,10 +97,6 @@ export default function FeaturedBlock({
                       >
                         <h3 className="text-lg font-semibold">{item.name}</h3>
                       </FontWrapper>
-                      {/* <p className="text-sm text-gray-500">
-                        {item.description}
-                      </p> */}
-                      {/* <p className="text-lg font-semibold">${item.price}</p> */}
                     </div>
                   </div>
                 </CardContent>
