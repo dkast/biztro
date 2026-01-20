028d00877a7e763fa66de448c5a5ecdbb91633c1
Daniel Castillejo <devcastillejo@gmail.com>
2026-01-19 19:32:44 -0600

    feat: enhance GlobalError component with improved styling and navigation

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -2,7 +2,24 @@
 
 import { useEffect } from "react"
 import * as Sentry from "@sentry/nextjs"
-import NextError from "next/error"
+import { Inter, Sora } from "next/font/google"
+import Link from "next/link"
+
+// import Footer from "@/components/marketing/footer"
+import Navbar from "@/components/marketing/nav-bar"
+import { Button } from "@/components/ui/button"
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter"
+})
+
+const sora = Sora({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-sora"
+})
 
 export default function GlobalError({
   error
@@ -10,17 +27,43 @@ export default function GlobalError({
   error: Error & { digest?: string }
 }) {
   useEffect(() => {
+    console.error(error)
     Sentry.captureException(error)
   }, [error])
 
   return (
-    <html lang="es-MX">
-      <body>
-        {/* `NextError` is the default Next.js error page component. Its type
-        definition requires a `statusCode` prop. However, since the App Router
-        does not expose status codes for errors, we simply pass 0 to render a
-        generic error message. */}
-        <NextError statusCode={0} />
+    <html
+      className={`${inter.variable} ${sora.variable} scroll-smooth antialiased`}
+      lang="es-MX"
+    >
+      <body className="bg-white text-gray-950 dark:bg-gray-950 dark:text-white">
+        <div className="flex h-dvh w-full flex-col">
+          <Navbar />
+          <div
+            className="flex h-96 grow flex-col items-center justify-center gap-6
+              sm:gap-12"
+          >
+            <h1
+              className="font-display text-6xl font-medium text-gray-800
+                slashed-zero sm:text-8xl dark:text-gray-200"
+            >
+              Algo salió mal
+            </h1>
+            <div className="space-y-2 text-center text-gray-500">
+              <h2 className="text-2xl sm:text-3xl">
+                Error interno del servidor
+              </h2>
+              <p>
+                Ocurrió un error inesperado. Por favor, intenta nuevamente más
+                tarde.
+              </p>
+            </div>
+            <Link href="/">
+              <Button>Volver al inicio</Button>
+            </Link>
+          </div>
+          <div className="w-full">{/* <Footer /> */}</div>
+        </div>
       </body>
     </html>
   )
