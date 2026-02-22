8d8fc93230931757e9925c104ffeadfcc6da92f5
Daniel Castillejo <devcastillejo@gmail.com>
2026-02-22 01:32:45 -0600

    feat: update DrawerContent to include a handle for improved user interaction

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -45,12 +45,15 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "bg-background fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border",
+        `bg-background fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col
+        rounded-t-[10px] border`,
         className
       )}
       {...props}
     >
-      <div className="bg-muted mx-auto mt-4 h-2 w-[100px] rounded-full" />
+      <DrawerPrimitive.Handle
+        className="bg-muted mx-auto mt-4 h-2 w-[100px] rounded-full"
+      />
       {children}
     </DrawerPrimitive.Content>
   </DrawerPortal>
