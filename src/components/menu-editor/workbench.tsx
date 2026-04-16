691c85d7e29fa62a6b60947698882786448302cd
Daniel Castillejo <devcastillejo@gmail.com>
2026-04-15 23:47:05 -0500

    fix: update ResizablePanel props to use panelRef instead of ref

--- src/components/menu-editor/workbench.tsx
+++ src/components/menu-editor/workbench.tsx
@@ -882,7 +882,7 @@ export default function Workbench({
           >
             <ResizablePanel
               id="left"
-              ref={leftPanelRef}
+              panelRef={leftPanelRef}
               defaultSize={isDataGridView ? "0%" : "18%"}
               minSize="15%"
               maxSize="25%"
@@ -911,7 +911,7 @@ export default function Workbench({
             {!isDataGridView && <ResizableHandle />}
             <ResizablePanel
               id="data-grid"
-              ref={dataGridPanelRef}
+              panelRef={dataGridPanelRef}
               defaultSize={isDataGridView ? "70%" : "0%"}
               collapsible
             >
@@ -1015,7 +1015,7 @@ export default function Workbench({
             {!isDataGridView && <ResizableHandle />}
             <ResizablePanel
               id="right"
-              ref={rightPanelRef}
+              panelRef={rightPanelRef}
               defaultSize={isDataGridView ? "0%" : "18%"}
               minSize="15%"
               maxSize="25%"
