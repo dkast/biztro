f4d4f538429e760f0a8a8d2084cf5338ef147323
Daniel Castillejo <devcastillejo@gmail.com>
2026-07-29 00:56:20 -0500

    chore: remove unused runtime export from opengraph-image.tsx

--- src/app/menu-internal/[subdomain]/opengraph-image.tsx
+++ src/app/menu-internal/[subdomain]/opengraph-image.tsx
@@ -7,7 +7,6 @@ import sharp from "sharp"
 
 import { getOrganizationBySlug } from "@/server/actions/organization/queries"
 
-export const runtime = "nodejs"
 export const size = { width: 1200, height: 630 }
 export const contentType = "image/png"
 
